93dc8d299baf1530e2d74fc3c9364c2b790b146a
Chris Ackerman <chris@topher.land>
2024-04-09 02:00:58 -0700

    removed unnecessary async closure from plugin/app/src/index.tsx

--- packages/plugin/app/src/index.tsx
+++ packages/plugin/app/src/index.tsx
@@ -3,6 +3,4 @@ import { createRoot } from 'react-dom/client';
 const app = document.getElementById('app')!;
 const root = createRoot(app);
 
-void (async () => {
-  root.render(<div>Hello, inspector!</div>);
-})();
+root.render(<div>Hello, inspector!</div>);
